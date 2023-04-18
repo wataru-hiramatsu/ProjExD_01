d81e70830f06d03a110cb1dc25b652bfc7015f9f
shadero <shadero.vrc@gmail.com>
2023-04-18 15:00:56 +0900

    こうかとんが1秒毎に羽ばたくようにした

--- pygame_image.py
+++ pygame_image.py
@@ -11,18 +11,21 @@ def main():
     kk_img_lst = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]
 
     tmr = 0
-
+    kk_idx = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         tmr += 1
-        
+
         bg_x_count = tmr % 1600
         screen.blit(bg_img, [-bg_x_count, 0])
         screen.blit(bg_img, [1600 - bg_x_count, 0])
 
-        screen.blit(kk_img_lst[tmr % len(kk_img_lst)], [300, 200])
+        if tmr % 100 == 0:
+            kk_idx = (kk_idx + 1) % len(kk_img_lst)
+
+        screen.blit(kk_img_lst[kk_idx], [300, 200])
 
         pg.display.update()
         clock.tick(100)

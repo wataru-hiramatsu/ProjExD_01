af7d4b84e3209fb712e4ec5f4c45cf62a4b9fd91
shadero <shadero.vrc@gmail.com>
2023-04-18 15:19:23 +0900

    リファクタリング

--- pygame_image.py
+++ pygame_image.py
@@ -5,7 +5,10 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
+    # 背景
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+    bg_img_lst = [bg_img, pg.transform.flip(bg_img, True, False)]
+    # こうかとん
     kk_img = pg.image.load("ex01/fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)
     kk_img_lst = [kk_img, pg.transform.rotozoom(kk_img, 10, 1.0)]
@@ -18,13 +21,14 @@ def main():
 
         tmr += 1
 
+        # 背景の描画
         bg_x_count = tmr % 1600
-        screen.blit(bg_img, [-bg_x_count, 0])
-        screen.blit(pg.transform.flip(bg_img, True, False), [1600 - bg_x_count, 0])
+        screen.blit(bg_img_lst[0], [-bg_x_count, 0])
+        screen.blit(bg_img_lst[1], [1600 - bg_x_count, 0])
 
+        # こうかとんの描画
         if tmr % 100 == 0:
             kk_idx = (kk_idx + 1) % len(kk_img_lst)
-
         screen.blit(kk_img_lst[kk_idx], [300, 200])
 
         pg.display.update()
